9fb0a0a00fbaf1bf39741c52b19e0ba6f1bc980d
Ahamed Aathil <ahamedaathil.5@gmail.com>
2024-02-12 18:50:26 +0530

    complete header and start footer
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,19 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="py-3"></footer>
+      <footer className="py-3"></footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy;{new Date().getFullYear()} Powered By Ahamed Aathil
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,160 @@
+import { NavLink, Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">Free Shipping & Free Returns</p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a className="text-white" href="tel:+94766611917">
+                  +94 76 66 119 17
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white">TFStore.</Link>
+              </h2>
+            </div>
+            <div className="col-5">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Product Here.."
+                  aria-label="Search Product Here.."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
+                </span>
+              </div>
+            </div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/compare.svg" alt="compare" />
+                    <p className="mb-0">
+                      Compare
+                      <br />
+                      Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/heart.svg" alt="wishlist" />
+                    <p className="mb-0">
+                      Add to
+                      <br />
+                      Wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/user.svg" alt="user" />
+                    <p className="mb-0">
+                      Login
+                      <br />
+                      User
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">$ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center gap-30">
+                <div className="">
+                  <div className="dropdown">
+                    <button
+                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15  d-flex align-items-center"
+                      type="button"
+                      id="dropdownMenuButton1"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img
+                        src="images/menu.svg"
+                        style={{ width: "20px", height: "20px" }}
+                        alt="menu"
+                      />
+                      <span className="me-5 d-inline-block ">
+                        Show Categories
+                      </span>
+                    </button>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="dropdownMenuButton1"
+                    >
+                      <li>
+                        <Link className="dropdown-item text-white" to="#">
+                          Action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="#">
+                          Another action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" to="#">
+                          Something else here
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink className="text-white" to={"/"}>
+                      Home
+                    </NavLink>
+                    <NavLink className="text-white" to={"/products"}>
+                      Products
+                    </NavLink>
+                    <NavLink className="text-white" to={"/aboutus"}>
+                      About us
+                    </NavLink>
+                    <NavLink className="text-white" to={"/help"}>
+                      Help
+                    </NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
